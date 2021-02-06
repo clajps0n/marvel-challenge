1657ccb20a6577cf2d3420c89b2e95d9662537ab
Cristian <amado.jaimes.cristian@gmail.com>
2021-02-06 09:33:41 -0500

    Website layout

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout'
-
+import { WavesModule, InputsModule, ButtonsModule, IconsModule } from 'angular-bootstrap-md'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    WavesModule, InputsModule, ButtonsModule, IconsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ],
   exports: [
-    FlexLayoutModule
+    CommonModule,
+    FlexLayoutModule,
+    WavesModule, InputsModule, ButtonsModule, IconsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
